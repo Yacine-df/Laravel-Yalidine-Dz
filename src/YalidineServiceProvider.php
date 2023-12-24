b33c7fd05d070e7e1db0c477e2d71555217dcbd3
Yacine-df <ycn.diaf@gmail.com>
2023-12-24 14:26:38 +0100

    initial commit

---
+++ src/YalidineServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Yacinediaf\Yalidine;
+
+use Illuminate\Support\ServiceProvider;
+use Yalidine;
+
+class YalidineServiceProvider extends ServiceProvider
+{
+
+
+    public function register()
+    {
+        $this->app->singleton(Yalidine::class, function () {
+
+            return new Yalidine();
+
+        });
+    }
+
+
+    public function boot()
+    {
+        /**
+         * publish yalidine config file into the laravel config files
+         */
+        
+        $this->publishes([
+
+            __DIR__ . '/../config/yalidine.php' => config('yalidine.php')
+
+        ]);
+
+    }
+}
---
+++ src/config/yalidine.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    /**
+     * 
+     * Pull yalidine api key value from the .env file 
+     * 
+     */
+
+    'key' => config('YALIDINE_API_KEY')
+
+];
